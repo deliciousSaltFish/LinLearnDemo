64c994729fe9fa9b3b00165bace6ff784084f1a3
Administrator <496057338@qq.com>
2019-01-15 21:39:14 +0800

    单例模式:同时调用单例，获取单例对象的时候，可能会出现两个单例对象，出现资源的混乱/版本不一，可以使用锁，不直接产生一个单例，在类载入的时候new一个类，后续只返回这个类

--- DisgenDemo/src/com/james/Singleton/Client.java
+++ DisgenDemo/src/com/james/Singleton/Client.java
@@ -2,13 +2,13 @@ package com.james.Singleton;
 
 public class Client {
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getSingleton();
+        Singleton singleton = Singleton.getInstance();
         singleton.showInfor();
         //如下写法会报错，不能直接调用构造方法，保证单例模式
 //        Singleton singleton1 = new Singleton();
-        singleton = Singleton.getSingleton();
+        singleton = Singleton.getInstance();
         singleton.showInfor();
-        singleton = Singleton.getSingleton();
+        singleton = Singleton.getInstance();
         singleton.showInfor();
     }
 }
--- DisgenDemo/src/com/james/Singleton/Singleton.java
+++ DisgenDemo/src/com/james/Singleton/Singleton.java
@@ -1,22 +1,32 @@
 package com.james.Singleton;
 
 public class Singleton {
-    private static Singleton singleton=null;
+    //解决1.中的问题，可以使用锁
+    private static Singleton singleton = new Singleton("James", "26", "男");
     private String name;
     private String age;
     private String sex;
-    private Singleton(String name,String age,String sex){
-     this.name=name;
-     this.age=age;
-     this.sex=sex;
+
+    private Singleton(String name, String age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
     }
-    public static Singleton getSingleton(){
-        if (singleton==null){
-            singleton=new Singleton("James", "26", "男");
-        }
+
+    //解决1.中的问题，可以使用锁
+    public synchronized static Singleton getInstance() {
         return singleton;
     }
-    public  void showInfor(){
-        System.out.println("----"+this.name);
+
+//1.如下方法会同时调用，判断条件会出现问题，可能会出现同时存在两个单例对象的情况
+//    public static Singleton getSingleton(){
+//        if (singleton==null){
+//            singleton=new Singleton("James", "26", "男");
+//        }
+//        return singleton;
+//    }
+
+    public void showInfor() {
+        System.out.println("----" + this.name);
     }
 }
